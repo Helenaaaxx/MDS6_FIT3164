5ec86defc5f297cca9741e93cfecb7baf99da487
mboo0012 <147753727+mboo0012@users.noreply.github.com>
2024-04-07 01:20:36 +0000

    Inital web app design for the project

---
+++ Predictive Model Web Application/Web App/src/page.js
@@ -0,0 +1,8 @@
+import Header from "./header";
+export default function Home() {
+  return (
+    <main className = "flex min-h-screen flex-col items-center justify-center bg-gray-100">
+        <Header />
+    </main>
+  );
+}
