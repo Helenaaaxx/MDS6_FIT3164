c8e4a341615394ec1a7afbc574929b6d2700094c
yngg0068 <yngg0068@student.monash.edu>
2024-04-30 23:06:42 +0800

    first version of design

--- Predictive Model Web Application/src/web.js
+++ Predictive Model Web Application/src/web.js
@@ -53,3 +53,111 @@ document.getElementById('makePredictionButton').addEventListener('click', functi
   // Use cancerType to make a prediction, likely involving an API call or another form of data processing
   // Then update the prediction results and accuracy
 });
+
+
+
+
+  (function(){
+    var DELIMETER =',';
+    var NEWLINE='\n';
+    var filePath ="preprocessed_datasets/Drug_Breast_Cancer_Dataset.csv";
+    var filename = filePath.split('/').pop();
+    var file = new File([filename], filePath, { type: "csv" });
+    var i = document.getElementById("file");
+    var table = document.getElementById('table-container');
+  
+    if(!i){
+      return;
+    }
+
+    i.addEventListener("change", function (){
+      if(!!i.files && i.files.length >0){
+        parseCSV(i.files[0]);
+
+      }
+    });
+
+    function parseCSV(file){
+      if(!file || !FileReader){
+        return;
+      }
+
+      var reader = new FileReader();
+
+      reader.onload =  function(e){
+
+        toTable(e.target.result);
+      }
+
+      reader.readAsText(file);
+
+    }
+
+    function toTable(text){
+
+      if(!text || ! table){
+        return;
+      }
+
+      while(!!table.lastElementChild){
+        table.removeChild(table.lastElementChild);
+      }
+
+      var rows = text.split(NEWLINE);
+      var headers =  rows.shift().trim().split(DELIMETER);
+
+      var htr = document.createElement('tr');
+      headers.forEach(function (h) {
+        
+          var th = document.createElement('th');
+          var ht = h.trim();
+        
+
+        if(!ht){
+          return;
+        }
+
+        th.textContent = ht;
+        htr.appendChild(th);
+        
+      });
+
+      table.appendChild(htr);
+
+      var rtr;
+
+      rows.forEach(function (r){
+
+        r= r.trim();
+
+        if(!r){
+          return;
+        }
+
+        var cols = r.split(DELIMETER);
+
+        if(cols.length ===0){
+          return;
+        }
+
+        rtr = document.createElement('tr');
+
+        cols.forEach(function (c){
+          var td = document.createElement('td');
+          var tc = c.trim();
+
+          td.textContent = tc;
+          rtr.appendChild(td);
+
+        });
+        table.appendChild(rtr);
+      });
+
+    }
+    
+  })();
+
+
+
+  
+
